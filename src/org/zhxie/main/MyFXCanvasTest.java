21a41434a8f40406274a4e3a57182695680d84bf
zhxie <zhxie@LT323278.asml.com>
2017-10-17 18:03:19 +0800

    refactor the fx composite

--- src/org/zhxie/main/MyFXCanvasTest.java
+++ src/org/zhxie/main/MyFXCanvasTest.java
@@ -1,97 +1,38 @@
 package org.zhxie.main;
 
 
-import java.io.IOException;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.zhxie.component.OldFXCanvas;
 import org.zhxie.component.Recorder;
 import org.zhxie.component.SWTTestUtil;
 
 import com.google.common.base.Function;
 
-import javafx.animation.AnimationTimer;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-
 public class MyFXCanvasTest {
-
-
-  private final static long[] frameTimes = new long[100];
-  private static int frameTimeIndex = 0 ;
-  private static boolean arrayFilled = false ;
 
   public static void main(String[] args) {
     open(800, 600);
   }
 
   private static void open(final int width, final int height) {
-    SWTTestUtil.openShell(String.format("FX Canvas (%dx%d)", width, height),
-        width+16, height+39, new Function<Shell, Control>() {
+    SWTTestUtil.openShell(String.format("FX Canvas (%dx%d)", width, height), width, height,
+        new Function<Shell, Control>() {
 
       @Override
       public Control apply(Shell shell) {
-        OldFXCanvas fxCanvas = new OldFXCanvas(shell, SWT.NONE);
-        AnchorPane root = getRoot();
-
-        final Label label = new Label();
-        AnimationTimer frameRateMeter = new AnimationTimer() {
-
-          @Override
-          public void handle(long now) {
-            long oldFrameTime = frameTimes[frameTimeIndex] ;
-            frameTimes[frameTimeIndex] = now ;
-            frameTimeIndex = (frameTimeIndex + 1) % frameTimes.length ;
-            if (frameTimeIndex == 0) {
-              arrayFilled = true ;
-            }
-            if (arrayFilled) {
-              long elapsedNanos = now - oldFrameTime ;
-              long elapsedNanosPerFrame = elapsedNanos / frameTimes.length ;
-              double frameRate = 1_000_000_000.0 / elapsedNanosPerFrame ;
-              label.setText(String.format("%.3f", frameRate));
-            }
-          }
-        };
-
+        Composite container = new Composite(shell, SWT.NONE);
+        FXCanvasComposite fxCanvas = new FXCanvasComposite(container, SWT.NONE, shell);
         new Thread(new AsyRunnableTask(shell,10000,600,400)).start();
         new Thread(new AsyRunnableTask(shell,20000,300, 200)).start();
-
-        label.textProperty().addListener(e -> {
-          Recorder.log(width, height, label.textProperty().getValue());
-        });
-        frameRateMeter.start();
-        VBox vBox = new VBox();
-        root.setPrefHeight(height);
-        root.setPrefWidth(width);
-        vBox.getChildren().addAll(root, label);
-        Scene scene = new Scene(vBox);
-        fxCanvas.setScene(scene);
         return fxCanvas;
       }
+
     });
     Recorder.close();
-  }
-
-  protected static AnchorPane getRoot() {
-    FXMLLoader fxmlLoader = new FXMLLoader(MyFXCanvasTest.class.getResource("textControl.fxml"));
-    //    fxmlLoader.setController(this);
-    //    fxmlLoader.setResources(getResourceBundle());
-
-    AnchorPane root = null;
-    try {
-      root = fxmlLoader.load();
-    } catch (IOException e) {
-      //      LOGGER.error(e.getMessage());
-    }
-    return root;
   }
 
   public static class AsyRunnableTask implements Runnable {
@@ -116,8 +57,10 @@ public class MyFXCanvasTest {
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
-      Display.getDefault().syncExec(()->{
-        shell.setSize(new Point(width,height));
+      Display.getDefault().syncExec(() -> {
+        if (!shell.isDisposed()) {
+          shell.setSize(new Point(width, height));
+        }
       });
     }
   }
